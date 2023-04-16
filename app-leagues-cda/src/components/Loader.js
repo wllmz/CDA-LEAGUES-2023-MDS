bbea9cfdfdd4fd41cb560a97a022386189b73b9b
wllmz <wiwimarti0693@gmail.com>
2023-04-16 17:39:06 +0200

    Create Loader.js
    Loader create on components

---
+++ app-leagues-cda/src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoaderGif from '../assets/img/ekko-lol.gif'
+
+export default function Loader() {
+  return (
+    <div className='loader'>
+        <img src={LoaderGif}/>
+
+
+    </div>
+  )
+}
